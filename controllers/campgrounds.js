a2a93af98b3a378463d3691239e302c7e7f107b7
bolattt <bolattny@gmail.com>
2022-01-17 19:33:07 -0500

    add map on show page

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,6 +1,9 @@
 const campground = require('../models/campground')
 const Campground = require('../models/campground')
 const {cloudinary} = require('../cloudinary')
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding")
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({accessToken:mapBoxToken})
 
 module.exports.index = async (req,res) => {
     const camps = await Campground.find()
@@ -13,7 +16,12 @@ module.exports.renderNewForm = (req,res) => {
 }
 
 module.exports.createCampground = async (req,res,next) => {   
+    const geoData = await geocoder.forwardGeocode({
+        query:req.body.campground.location,
+        limit:1
+    }).send()
     const camp = new Campground(req.body.campground)
+    camp.geometry = geoData.body.features[0].geometry;
     camp.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
     camp.author = req.user._id;
     await camp.save();
